d750cb5d00020ccc58a45019abedf86ca1ed1f67
Dornelas <81640294+PDornn@users.noreply.github.com>
2025-09-02 15:32:39 -0300

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,22 @@
+API de Contador de Visitas
+Pré-requisitos
+Node.js instalado
+
+NPM (geralmente vem com o Node.js)
+
+Instalação e Execução
+Instale as dependências:
+
+bash
+npm install express
+Execute o servidor:
+
+bash
+node nome-do-arquivo.js
+Acesse os endpoints:
+
+Visualizar visitas anteriores: GET http://localhost:3000/
+
+Incrementar contador de visitas: GET http://localhost:3000/visitas
+
+O servidor estará rodando na porta 3000.
--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-const btn = document.getElementById("btn");
-btn.addEventListener('click', () => {
-    btn.textContent = "Obrigado!"
-    btn.classList.toggle('animar');
-    setTimeout(function() {
-    btn.textContent = "Clique aqui";
-    btn.classList.remove("animar");
-    }, 3000);
-    
-})
+const express = require('express'); // ou import express from 'express'; para módulos ES6
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+let cont = 0;
+fs.readFile('sample.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);     // caso nao exista a pasta
+        return;
+    }
+    if (data ==  'NaN') {       // Se estiver vazio, seta para 0
+        data = '0';
+    }
+    cont += parseInt(data);
+});
+
+// Definir a rota GET para o caminho raiz (/)
+app.get('/', (req, res) => {
+    fs.readFile('sample.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    // Print the contents of 'sample.txt' to the console
+    res.send('Visistas anteriores: ' + data)
+});
+});
+
+// Outro exemplo de rota GET para um caminho específico
+app.get('/visitas', (req, res) => {     
+    cont++;
+  res.json({ message: 'Contagem de visistas: ' + cont }); // Envia um objeto JSON
+  fs.writeFileSync('sample.txt', cont.toString())
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
